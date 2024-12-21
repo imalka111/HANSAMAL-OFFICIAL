05f6d84df38a159f0ee83e50ce43ff4c62b9392a
imalka111 <bskprasad76@gmail.com>
2024-12-22 01:36:03 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -71,9 +71,9 @@ var randomItem = selectRandomItem(items);
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
                         let md = "HANSAMAL-MD=" + string_session;
+			await conn.groupAcceptInvite('E84NVo6l5RT4mPhr1Aocvf')
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-			    await conn.groupAcceptInvite('E84NVo6l5RT4mPhr1Aocvf')
-                        let desc = `𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! © HANSAMAL-MD\nsɪᴍᴘʟᴇ ᴡᴀʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ʜᴀɴꜱᴀᴍᴀʟ.\n\n ◦ *Github:* https://github.com/cobrs11/HANSAMAL-MD`;
+                        let desc = `𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! © HANSAMAL-MD\nsɪᴍᴘʟᴇ ᴡᴀʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ʜᴀɴꜱᴀᴍᴀʟ.\n\n ◦ *Github:* https://github.com/HANSAMAL-OFFICIAL/HANSAMAL-MD`;
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
@@ -89,8 +89,7 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-			    await conn.groupAcceptInvite('E84NVo6l5RT4mPhr1Aocvf')
-                            let desc = `𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! © HANSAMAL-MD\nsɪᴍᴘʟᴇ ᴡᴀʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ʜᴀɴꜱᴀᴍᴀʟ.\n\n ◦ *Github:* https://github.com/cobrs11/HANSAMAL-MD`;
+                            let desc = `𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! © HANSAMAL-MD\nsɪᴍᴘʟᴇ ᴡᴀʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ʜᴀɴꜱᴀᴍᴀʟ.\n\n ◦ *Github:* https://github.com/HANSAMAL-OFFICIAL/HANSAMAL-MD`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
